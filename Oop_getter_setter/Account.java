b61c979d60036eebe4abce71185dce323b2b1e13
unknown <rvpatidar12@.com>
2022-05-18 19:01:24 +0530

    programs

---
+++ Oop_getter_setter/Account.java
@@ -0,0 +1,36 @@
+package Oop_getter_setter;
+
+public class Account {
+	private String number;
+	private String accountType;
+	private double balance;
+	
+	public String getNumber(){
+		return number;
+	}
+	public void setNumber(String n) {
+		number = n;
+	}
+	public String getAccountType(){
+		return accountType;
+	}
+	public void setAccountType(String at){
+		accountType = at;
+	}
+	public void deposit(double d){
+		balance = balance+d;
+	}
+	public void withdrawal(double d){
+		 balance = balance-d;
+	}
+	public double getBalance() {
+		return balance;
+	}
+	public void fundTransfer(double d){
+		balance = balance-d;
+	}
+	public void payBill(double d){
+		balance = balance-d;
+	}
+	
+}
---
+++ Oop_getter_setter/PersonInformation.java
@@ -0,0 +1,55 @@
+package Oop_getter_setter;
+import java.text.ParseException;
+public class PersonInformation {
+	public static void main(String[] args) throws Exception {
+		Person p = new Person();
+		p.setName("Ravi");
+		p.setAdress("Indore");
+		p.setDob("02/09/1996");
+		
+		Account a = new Account();
+		a.setNumber("0123456789");
+		a.setAccountType("Savings");
+		a.deposit(5034567.67);
+		a.withdrawal(2000);
+		a.fundTransfer(50000);
+		a.payBill(54);
+		
+		Automobile am = new Automobile();
+		am.setMake("Rolls Royce");
+		am.setColor("white");
+		am.changeGear(3);
+		am.breck(60);
+		am.accelerator(20);
+		
+		
+	    p.getName();
+		p.getAdress();
+		p.getDob();
+		
+		a.getNumber();
+		a.getAccountType();
+		a.getBalance();
+		
+		am.getColor();
+		am.getMake();
+		am.getSpeed();
+		
+		
+		System.out.println("Person's Name :- "+p.getName());
+		System.out.println("Adress :- "+p.getAdress());
+		System.out.println("Date Of Birth :- "+p.getDob());
+		System.out.println();
+		
+		System.out.println("Account Number :- "+a.getNumber());
+		System.out.println("Account Type :- "+a.getAccountType());
+		System.out.println("Remaining Balance :- "+a.getBalance());
+		System.out.println();
+		
+		
+		System.out.println("Vehicel Name :- "+am.getMake());
+		System.out.println("Vehicel Color :- "+am.getColor());
+		System.out.println("Cureent Speed :- "+am.getSpeed());
+		}
+
+}
